0a21af2dfe593e951822b872a9ec8e01f2d551ad
uday thanki <udaythanki2@gmail.com>
2024-11-02 22:33:48 +0530

    updated the Project component

--- src/Constant/ProjectDetails.js
+++ src/Constant/ProjectDetails.js
@@ -7,31 +7,31 @@ import image5 from '../assets/projects/project-5.png'
 const projects = [
     {
       title: "Real-Time Chat Application",
-      image: image1, // Replace with the actual path or URL of the image
+      image: "../src/assets/projects/project-1.png", // Replace with the actual path or URL of the image
       description: "A chat application that allows real-time communication between users. It features user authentication, and live messaging using Socket.io for seamless interactions.",
       technologies: ["MongoDB", "Express.js", "React", "Node.js", "Socket.io", "Bootstrap"]
     },
     {
       title: "Weather App",
-      image: image2, // Replace with the actual path or URL of the image
+      image: "../src/assets/projects/project-2.png", // Replace with the actual path or URL of the image
       description: "A web application that provides weather updates based on user location or city input. It fetches real-time data from a weather API, displaying temperature, humidity, and weather conditions in a user-friendly interface.",
       technologies: ["React", "JavaScript", "CSS", "HTML5"]
     },
     {
       title: "Employee Management System",
-      image: image3, // Replace with the actual path or URL of the image
+      image: "../src/assets/projects/project-3.png", // Replace with the actual path or URL of the image
       description: "An employee management system that performs CRUD operations. It features employee data , updating, and deletion. The system includes login authentication and an intuitive dashboard for managing employee records.",
       technologies: ["MongoDB", "Express.js", "React", "Node.js", "Bootstrap"]
     },
     {
       title: "Search App",
-      image: image4, // Replace with the actual path or URL of the image
+      image: "../src/assets/projects/project-4.png", // Replace with the actual path or URL of the image
       description: "A search application that integrates results from YouTube, blogs, articles, and academic papers. It filters and ranks search results based on relevance, views, and likes to ensure users get the most pertinent content.",
       technologies: ["React", "JavaScript", "CSS", "API Integration", "HTML5"]
     },
     {
       title: "Recipe App",
-      image: image5, // Replace with the actual path or URL of the image
+      image: "../src/assets/projects/project-5.png", // Replace with the actual path or URL of the image
       description: "A recipe application that allows users to search for recipes,  and view detailed cooking instructions. This app uses an API to fetch a vast collection of recipes, making meal planning easy and enjoyable for users.",
       technologies: ["React", "Node.js", "API Integration", "CSS", "HTML5"]
     }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import projectDetails from '../Constant/ProjectDetails.js'
 import { motion } from 'framer-motion'
+import image1 from '../assets/projects/project-1.png'
+import image2 from '../assets/projects/project-2.png'
+import image3 from '../assets/projects/project-3.png'
+import image4 from '../assets/projects/project-4.png'
+import image5 from '../assets/projects/project-5.png'
+
 const Projects = () => {
     return (
         <div className='border-b border-purple-800 pb-4'>
@@ -9,44 +14,124 @@ const Projects = () => {
                 initial={{ opacity: 0, y: -100 }}
                 transition={{ duration: 0.5 }}
                 className='my-20 text-center text-4xl'>Projects</motion.h2>
-            <div>
-                {projectDetails.map((Projects, index) => {
-                    return (
-                        <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
-                            <motion.div
-                                whileInView={{ opacity: 1, x: 0 }}
-                                initial={{ opacity: 0, x: -100 }}
-                                transition={{ duration: 1 }}
-                                className='w-full lg:w-1/4'>
-                                <img src={Projects.image}
-                                    alt="Project image"
-                                    width={150}
-                                    height={150}
-                                    className='mb-6 rounded'
-                                />
-                            </motion.div>
-                            <motion.div
-                                whileInView={{ opacity: 1, x: 0 }}
-                                initial={{ opacity: 0, x: 100 }}
-                                transition={{ duration: 1 }}
-                                className='w-full max-w-xl lg:w-3/4'>
-                                <h6 className='mb-2 font-semibold'>{Projects.title}</h6>
-                                <p className='mb-4 text-neutral-300'>{Projects.description}</p>
-                                {Projects.technologies.map((tech, index) => {
-                                    return (
-                                        <span
-                                            key={index}
-                                            className='mr-2 rounded bg-neutral-900 px-2 py-1 sm:py-3 text-sm font-medium text-yellow-400 
-               [text-shadow:0px_0px_3px_#e6b800, 0px_0px_5px_#e6b800, 0px_0px_8px_#8000ff, 0px_0px_10px_#8000ff]'>
-                                            {tech}
-                                        </span>
+            
+            {/* Real-Time Chat Application */}
+            <div className='mb-8 flex flex-wrap lg:justify-center'>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full lg:w-1/4'>
+                    <img src={image1} alt="Real-Time Chat Application" width={150} height={150} className='mb-6 rounded' />
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full max-w-xl lg:w-3/4'>
+                    <h6 className='mb-2 font-semibold'>Real-Time Chat Application</h6>
+                    <p className='mb-4 text-neutral-300'>A chat application that allows real-time communication between users. It features user authentication, and live messaging using Socket.io for seamless interactions.</p>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>MongoDB</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Express.js</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>React</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Node.js</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Socket.io</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Bootstrap</span>
+                </motion.div>
+            </div>
 
-                                    )
-                                })}
-                            </motion.div>
-                        </div>
-                    )
-                })}
+            {/* Weather App */}
+            <div className='mb-8 flex flex-wrap lg:justify-center'>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full lg:w-1/4'>
+                    <img src={image2} alt="Weather App" width={150} height={150} className='mb-6 rounded' />
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full max-w-xl lg:w-3/4'>
+                    <h6 className='mb-2 font-semibold'>Weather App</h6>
+                    <p className='mb-4 text-neutral-300'>A web application that provides weather updates based on user location or city input. It fetches real-time data from a weather API, displaying temperature, humidity, and weather conditions in a user-friendly interface.</p>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>React</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>JavaScript</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>CSS</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>HTML5</span>
+                </motion.div>
+            </div>
+
+            {/* Employee Management System */}
+            <div className='mb-8 flex flex-wrap lg:justify-center'>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full lg:w-1/4'>
+                    <img src={image3} alt="Employee Management System" width={150} height={150} className='mb-6 rounded' />
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full max-w-xl lg:w-3/4'>
+                    <h6 className='mb-2 font-semibold'>Employee Management System</h6>
+                    <p className='mb-4 text-neutral-300'>An employee management system that performs CRUD operations. It features employee data, updating, and deletion. The system includes login authentication and an intuitive dashboard for managing employee records.</p>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>MongoDB</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Express.js</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>React</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Node.js</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>Bootstrap</span>
+                </motion.div>
+            </div>
+
+            {/* Search App */}
+            <div className='mb-8 flex flex-wrap lg:justify-center'>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full lg:w-1/4'>
+                    <img src={image4} alt="Search App" width={150} height={150} className='mb-6 rounded' />
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full max-w-xl lg:w-3/4'>
+                    <h6 className='mb-2 font-semibold'>Search App</h6>
+                    <p className='mb-4 text-neutral-300'>A search application that integrates results from YouTube, blogs, articles, and academic papers. It filters and ranks search results based on relevance, views, and likes to ensure users get the most pertinent content.</p>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>React</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>JavaScript</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>CSS</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>API Integration</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>HTML5</span>
+                </motion.div>
+            </div>
+
+            {/* Recipe App */}
+            <div className='mb-8 flex flex-wrap lg:justify-center'>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full lg:w-1/4'>
+                    <img src={image5} alt="Recipe App" width={150} height={150} className='mb-6 rounded' />
+                </motion.div>
+                <motion.div
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                    className='w-full max-w-xl lg:w-3/4'>
+                    <h6 className='mb-2 font-semibold'>Recipe App</h6>
+                    <p className='mb-4 text-neutral-300'>An interactive recipe application that provides users with various recipes, ingredients, and cooking steps. It includes search and filter options to help users quickly find desired recipes based on ingredients or cuisine.</p>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>React</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>JavaScript</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>CSS</span>
+                    <span className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-yellow-400'>HTML5</span>
+                </motion.div>
             </div>
         </div>
     )
